d074c9c3e5dfc8ea06dea02a77b8e183b555d8ec
KokiOkuda <165885793+penguin0526@users.noreply.github.com>
2024-05-19 00:17:49 +0000

    [Update 2.2]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,9 +34,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
 
